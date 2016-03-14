931d3b488dae1a4d7381239c336bc8fcd39ab32c
Mathias Graae <Mathias Graae>
2016-03-14 19:39:09 +0100

    fixing for heroku

--- routes/readFile.js
+++ routes/readFile.js
@@ -3,7 +3,7 @@
 var express = require('express'),
     ReadFileController = express.Router(),
     multer = require('multer'),
-    upload = multer({dest: "../temp/"});
+    upload = multer({dest: "../tmp/"});
     
 ReadFileController.post('/', upload.single('file'), (req, res) => {
     res.json({'size': req.file.size});
