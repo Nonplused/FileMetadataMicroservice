0e477d7c5674a043d3f33b2dfad5b63c7bc39443
Mathias Graae <Mathias Graae>
2016-03-14 12:09:51 +0100

    Setup UI

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 //dependencies
 var express = require('express'),
-    config = require('konfig');
+    config = require('konfig')();
 
 //express
 var port = config.app.port;
@@ -11,6 +11,9 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 //routes
+app.get('/', (req, res, err) => {
+  res.render('index', {})
+});
 
 //server
 app.listen(port, () => {
